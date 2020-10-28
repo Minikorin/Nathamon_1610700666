2d13fec9fac78f801cfd135107ef819f05c386ce
Minikorin <61245402+Minikorin@users.noreply.github.com>
2020-10-28 23:19:43 +0700

    Week3 Delegate

---
+++ Week3/Assets/Week3/Delegate/Scripts/Enemy.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:78fa89cd5d3b434bf18aa8633e6697f35320e75ad7b597e7c8f525119fd7b596
+size 793
---
+++ Week3/Assets/Week3/Delegate/Scripts/GameState.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c234e031a5de311662e5ac384333b3df51d7b4a63983d85464dfc841c58187f2
+size 1045

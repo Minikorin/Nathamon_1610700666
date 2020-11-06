26865785eb165d21221b51fb9fb0992d4513c13f
Minikorin <61245402+Minikorin@users.noreply.github.com>
2020-11-07 01:36:29 +0700

    Test week 10 Add ShopManager

---
+++ TestWeek10/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:589d1c5d0721a04b6ca20c19ca4ea33f5f02b93a93529a7bbc86cc7f85f2c95a
+size 2850
---
+++ TestWeek10/Assets/Scripts/UIManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5d5d99721ae1194a12bf61d9b86d443491e4b7efa20898a9692620c1c1cad2c
+size 177
